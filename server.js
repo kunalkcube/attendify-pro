121bcb5c1cb87bc90f8cf346802f6e6ffdaf1748
Kunal <kunalkashyap3010@gmail.com>
2024-01-18 15:04:02 +0530

    Attendify-Pro callback url add

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback',
+    callbackURL: 'https://attendify-pro.onrender.com/auth/google/callback',
 },
     async (accessToken, refreshToken, profile, done) => {
         // Generate a secret key using crypto
